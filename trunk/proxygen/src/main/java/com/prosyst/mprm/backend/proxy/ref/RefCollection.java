f1d77c9ebc3f677428a9684220bae21635f644de
rinswind <rinswind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2008-12-17 11:11:10 +0000

    Added Guice to the mix. Added a test hello exporter that uses both Guice and Autowire.

--- trunk/proxygen/src/main/java/com/prosyst/mprm/backend/proxy/ref/RefCollection.java
+++ trunk/proxygen/src/main/java/com/prosyst/mprm/backend/proxy/ref/RefCollection.java
@@ -1,11 +1,12 @@
 package com.prosyst.mprm.backend.proxy.ref;
 
+import java.util.Collection;
 import java.util.Iterator;
 
-public interface RefCollection extends Ref {
-  void add(Ref r);
+public interface RefCollection<T> extends Ref<Collection<T>> {
+  void add(Ref<T> ref);
   
-  boolean remove(Ref r);
+  boolean remove(Ref<T> ref);
   
-  Iterator iterator();
+  Iterator<Ref<T>> iterator();
 }
--- trunk/test-exporter-guicy-hello/src/main/java/com/prosyst/mprm/backend/autowire/test/exporter/hello/HelloExporter.java
+++ trunk/test-exporter-guicy-hello/src/main/java/com/prosyst/mprm/backend/autowire/test/exporter/hello/HelloExporter.java
@@ -1,12 +1,17 @@
 package com.prosyst.mprm.backend.autowire.test.exporter.hello;
 
+import static com.google.inject.Guice.createInjector;
+
 import org.osgi.framework.Constants;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
 import com.prosyst.mprm.backend.autowire.dsl.RefContainerImpl;
-import com.prosyst.mprm.backend.proxy.ref.Ref;
-import com.prosyst.mprm.backend.proxy.ref.RefListener;
 import com.prosyst.mprm.backend.autowire.test.exporter.date.Date;
 import com.prosyst.mprm.backend.autowire.test.exporter.format.Format;
+import com.prosyst.mprm.backend.proxy.ref.Ref;
+import com.prosyst.mprm.backend.proxy.ref.RefListener;
+
 
 /**
  * @author Todor Boev
@@ -20,19 +25,24 @@ public class HelloExporter extends RefContainerImpl {
     final Format format = (Format) importer().of(Format.class).asSingleton().proxy();
     final Date date = (Date) importer().of(Date.class).asSingleton().proxy();
     
-    final Ref deps = and(format, date);
+    final Ref required = and(format, date);
+   
+    Injector injector = createInjector(new AbstractModule() {
+      @Override
+      protected void configure() {
+        bind(Format.class).toInstance(format);
+        bind(Date.class).toInstance(date);
+        bind(Hello.class).to(HelloImpl.class);
+      }
+    });
     
     for (int i = 0; i < NO; i++) {
       final int no = i;
-      final Hello hello = new Hello() {
-        public void hello(String name) {
-          System.out.println(format.format(date.get()) + format.format(name));
-        }
-      };
+      final Hello hello = injector.getInstance(Hello.class);
       
       Ref export = exporter().of(Hello.class).asSingleton();
       
-      from(deps)
+      from(required)
       .bind(export)
       .to(Hello.PROP, Integer.valueOf(no))
       .to(Constants.SERVICE_RANKING, Integer.valueOf(NO - no))
