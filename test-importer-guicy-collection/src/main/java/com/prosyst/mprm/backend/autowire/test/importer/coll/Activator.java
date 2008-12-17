553eb237ec8958ef26c8b1bac95cd4dacc557fa8
rinswind <rinswind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2008-12-17 13:25:56 +0000

    Made the Collections importer even more guicy. Tested the provider methods of Guice 2.0 along the way.

--- test-importer-guicy-collection/src/main/java/com/prosyst/mprm/backend/autowire/test/importer/coll/Activator.java
+++ test-importer-guicy-collection/src/main/java/com/prosyst/mprm/backend/autowire/test/importer/coll/Activator.java
@@ -3,19 +3,22 @@ package com.prosyst.mprm.backend.autowire.test.importer.coll;
 import static com.google.inject.Guice.createInjector;
 import static com.google.inject.Key.get;
 import static com.google.inject.Scopes.SINGLETON;
+import static com.google.inject.name.Names.named;
 
 import java.util.Collection;
 import java.util.Map;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
+import com.google.inject.Provides;
 import com.google.inject.TypeLiteral;
+import com.google.inject.name.Named;
 import com.prosyst.mprm.backend.autowire.ObjectFactory;
 import com.prosyst.mprm.backend.autowire.dsl.RefContainerImpl;
 import com.prosyst.mprm.backend.autowire.test.exporter.hello.Hello;
+import com.prosyst.mprm.backend.autowire.test.exporter.worker.Worker;
 
 /**
- * @author Todor Boev
  * @version $Revision$
  */
 public class Activator extends RefContainerImpl {
@@ -31,9 +34,10 @@ public class Activator extends RefContainerImpl {
           .asCollection()
           .withVal(RichHello.class)
           /*
-           * Here the DSL is not guice-friendly - it must take a class or a
-           * provider and use guice to create the factory object. Instead it
-           * forces the user to create it himself.
+           * Here the DSL is not Guice-friendly. It should take a class or a
+           * provider and use Guice to create the factory object. Instead it
+           * forces the user to create the object himself because this method
+           * requires an instance - not a class/provider.
            */
           .createdBy(new ObjectFactory() {
             public Object create(final Object delegate, Map props) {
@@ -50,7 +54,27 @@ public class Activator extends RefContainerImpl {
           .proxy()
         );
             
+        /*
+         * This must be a singleton because later we want to hook a signal from
+         * the collection of services to the printer.
+         */
         bind(Printer.class).in(SINGLETON);
+        /*
+         * The worker needs a runnable. Tell guice to use the printer as a
+         * source of Runnables.
+         */
+        bind(Runnable.class).to(Printer.class);
+        bind(String.class).annotatedWith(named("worker name")).toInstance("Printer worker");
+      }
+      
+      /*
+       * In case Worker constructor is not annotated with @Inject we can
+       * annotate a method to act as the constructor. Cool stuff from Guice 2.0.
+       */
+      @Provides
+      @SuppressWarnings("unused")
+      public Worker provideWorker(@Named("worker name") String name, Runnable task) {
+        return new Worker(name, task);
       }
     });
 
@@ -59,7 +83,7 @@ public class Activator extends RefContainerImpl {
      * services becomes available. A collection becomes available (and empty) as
      * soon as the bundle is started.
      * 
-     * Going around Java 5's dreded erasure in order to get type safety of
+     * Going around Java 5's dreaded erasure in order to get type safety of
      * generified collection requires the crufly TypeLiteral - alas it can't be
      * made shorter.
      */
