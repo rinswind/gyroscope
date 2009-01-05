9d529273a98cbc42f71b175e81368114c7ea1032
rinswind <rinswind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2009-01-05 13:54:12 +0000

    Trailing commit for the ObjectFactory->Transformer rename.

---
+++ branches/monadic-ref/proxygen/src/main/java/com/prosyst/mprm/backend/proxy/ref/RefListenerAdapter.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.prosyst.mprm.backend.proxy.ref;
+
+public class RefListenerAdapter implements RefListener {
+  public void bound() {
+    /* User code comes here */
+  }
+
+  public void unbinding() {
+    /* User code comes here */
+  }
+
+  public void updated() {
+    /* User code comes here */
+  }
+}
