9b7324dcb7ed28823f9eee74bd28df932e09b284
rinswind <rinswind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2009-01-11 17:32:22 +0000

    Added the Apache 2.0 license to all files.

--- trunk/test-exporter-hello/src/main/java/edu/unseen/autowire/test/exporter/hello/Goodbie.java
+++
@@ -1,7 +0,0 @@
-package edu.unseen.autowire.test.exporter.hello;
-
-import edu.unseen.autowire.test.exporter.hello.Hello;
-
-public interface Goodbie extends Hello {
-  void goodbie(String name);
-}
