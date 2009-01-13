51e04b8f0ce96f7a347de90c8ba8490c4cef1b42
rinsvind <rinsvind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2009-01-13 13:48:11 +0000

    Refactored the benchmark to match the one used by Peaberry. Appears that because of the simpler service tracking model Gyro's proxies are 10-20% faster than the proxies of Peaberry. If I drop the usage of the heavy Read-Write lock they should be even faster but this will disallow the bundle to call concurrently the proxies service and this is a decision that rests with the service implementor.

--- trunk/proxygen/src/test/java/org/unseen/proxy/ProxyPerfTest.java
+++ trunk/proxygen/src/test/java/org/unseen/proxy/ProxyPerfTest.java
@@ -18,6 +18,11 @@ package org.unseen.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
+import junit.framework.TestCase;
+
+import org.unseen.proxy.ExampleBenchmark.Example;
+import org.unseen.proxy.ExampleBenchmark.ExampleImpl;
+import org.unseen.proxy.ExampleBenchmark.SyncExampleImpl;
 import org.unseen.proxy.gen.Proxy;
 import org.unseen.proxy.gen.ProxyFactory;
 import org.unseen.proxy.impl.ProxyClassLoader;
@@ -26,65 +31,16 @@ import org.unseen.proxy.ref.Ref;
 import org.unseen.proxy.ref.RefImpl;
 import org.unseen.proxy.ref.Transformers;
 
-import junit.framework.TestCase;
-
-
 /**
  * @author Todor Boev
  * @version $Revision$
  */
 public class ProxyPerfTest extends TestCase {
   /**
-   * The dynamic proxies can have up to 50% worse performance than a regular
-   * synchronized method
-   */
-  private final int THRESHOLD = 60;
-  
-  public interface Sum {
-    void add(int i);
-    
-    void sub(int i);
-    
-    int get();
-  }
-  
-  public static class SumImpl implements Sum {
-    private int s;
-    
-    public void add(int i) {
-      s += i;
-    }
-
-    public void sub(int i) {
-      s -= i;
-    }
-    
-    public int get() {
-      return s;
-    }
-  }
-  
-  public static class SyncSumImpl implements Sum {
-    private int s;
-    
-    public synchronized void add(int i) {
-      s += i;
-    }
-
-    public synchronized void sub(int i) {
-      s -= i;
-    }
-    
-    public synchronized int get() {
-      return s;
-    }
-  }
-  
-  /**
    * 
    */
   public void testProxyControl() {
-    Sum dynamic = dynamic();
+    Example dynamic = dynamic();
     
     assertNotNull(((Proxy<?, ?>) dynamic).proxyControl());
   }
@@ -93,90 +49,67 @@ public class ProxyPerfTest extends TestCase {
    * 
    */
   public void testPerformance() {
-    int reps = 10000000; // 10 million! :)
+    int reps = 1000000; // 1 million! :)
     int warmup = reps;
     
-    ProxyPerfDriver control = new ProxyPerfDriver(control(), reps, warmup);
-    ProxyPerfDriver syncControl = new ProxyPerfDriver(syncControl(), reps, warmup);
-    ProxyPerfDriver manual = new ProxyPerfDriver(manual(), reps, warmup);
-    ProxyPerfDriver dynamic = new ProxyPerfDriver(dynamic(), reps, warmup);
-    ProxyPerfDriver reflexive = new ProxyPerfDriver(reflexive(), reps, warmup);
+    ExampleBenchmark base = new ExampleBenchmark("Base", base(), reps, warmup);
+    ExampleBenchmark sync = new ExampleBenchmark("Sync", sync(), reps, warmup);
+    ExampleBenchmark manual = new ExampleBenchmark("Manual", manual(), reps, warmup);
+    ExampleBenchmark dynamic = new ExampleBenchmark("Dynamic", dynamic(), reps, warmup);
+    ExampleBenchmark reflexive = new ExampleBenchmark("Reflexive", reflexive(), reps, warmup);
     
-    run("Control", control);
-    long syncTime = run("SyncControl", syncControl);
-    long dynamicTime = run("Dynamic", dynamic);
-    run("SyncReflexive", reflexive);
-    run("Manual", manual);
+    long baseTime = base.time();
     
-    long timeDiff = dynamicTime - syncTime;
-    float ratio = ((float) timeDiff)/syncTime;
-    int perc = (int) (100*ratio);
+    int manualOverhead = manual.benckmark(baseTime);
+    int syncOverhead = sync.benckmark(baseTime);
+    int dynamicOverhead = dynamic.benckmark(baseTime);
+    int reflexiveOverhead = reflexive.benckmark(baseTime);
     
-    System.out.println("Dynamic is " + perc + "% worse than synchronized");
-    
-    assertTrue("Difference between dynamic and syncronized is less than " + THRESHOLD + "%: "
-        + perc + "%", perc < THRESHOLD);
+    assertTrue(manualOverhead < syncOverhead);
+    assertTrue(syncOverhead < dynamicOverhead);
+    assertTrue(dynamicOverhead < reflexiveOverhead);
   }
   
-  private static long run(String name, ProxyPerfDriver test) {
-    long time = test.test();
-    
-    System.out.println(name + "(" + test.get() + "): " + time);
-    
-    return time;
+  private static Example base() {
+    return new ExampleImpl();
   }
   
-  private static Sum control() {
-    return new SumImpl();
+  private static Example sync() {
+    return new SyncExampleImpl();
   }
   
-  private static Sum syncControl() {
-    return new SyncSumImpl();
-  }
-  
-  private static Sum dynamic() {
-    Ref<Sum, Sum> ref = new RefImpl<Sum,Sum>(Transformers.<Sum>identity());
-    
-    ref.bind(new SumImpl(), null);
-    
-    ProxyClassLoader loader = new ProxyClassLoader(ProxyPerfTest.class.getClassLoader());
-    ProxyFactory fact = new ProxyFactoryImpl(loader);
-    
-    return fact.proxy(Sum.class, ref);
-  }
-  
-  /**
-   * @return
-   */
-  private static Sum manual() {
-    return new Sum() {
-      private final Sum delegate = new SumImpl();
+  private static Example manual() {
+    return new Example() {
+      private final Example delegate = new ExampleImpl();
       
-      public void add(int i) {
-        delegate.add(i);
-      }
-
-      public void sub(int i) {
-        delegate.sub(i);
-      }
-      
-      public int get() {
-        return delegate.get();
+      public double action(double id) {
+        return delegate.action(id);
       }
     };
   }
   
+  private static Example dynamic() {
+    Ref<Example, Example> ref = new RefImpl<Example, Example>(Transformers.<Example>identity());
+    
+    ref.bind(new ExampleImpl(), null);
+    
+    ProxyClassLoader loader = new ProxyClassLoader(ProxyPerfTest.class.getClassLoader());
+    ProxyFactory fact = new ProxyFactoryImpl(loader);
+    
+    return fact.proxy(Example.class, ref);
+  }
+  
   /**
    * @return
    */
-  private static Sum reflexive() {
-    return (Sum) java.lang.reflect.Proxy.newProxyInstance(
+  private static Example reflexive() {
+    return (Example) java.lang.reflect.Proxy.newProxyInstance(
         ProxyPerfTest.class.getClassLoader(),
-        new Class<?>[] {Sum.class}, 
+        new Class<?>[] {Example.class}, 
         new InvocationHandler() {
-          private final Sum delegate = new SumImpl();
+          private final Example delegate = new ExampleImpl();
           
-          public synchronized Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+          public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
             return method.invoke(delegate, args);
           }
         });
