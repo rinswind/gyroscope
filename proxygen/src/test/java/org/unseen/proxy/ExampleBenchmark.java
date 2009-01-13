3f5799a9c2cb742d41bf80683409ebde311f89fa
rinsvind <rinsvind@597aea4a-e0fa-11dd-a8eb-ddf07f3c1838>
2009-01-13 13:48:11 +0000

    Refactored the benchmark to match the one used by Peaberry. Appears that because of the simpler service tracking model Gyro's proxies are 10-20% faster than the proxies of Peaberry. If I drop the usage of the heavy Read-Write lock they should be even faster but this will disallow the bundle to call concurrently the proxies service and this is a decision that rests with the service implementor.

---
+++ proxygen/src/test/java/org/unseen/proxy/ExampleBenchmark.java
@@ -0,0 +1,95 @@
+/**
+ * Copyright (C) 2008 Todor Boev
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.unseen.proxy;
+
+
+/**
+ * @author Todor Boev
+ * @version $Revision$
+ */
+public class ExampleBenchmark {
+  private final String name;
+  private final Example tested;
+  private final int repeats;
+  private final int warmup;
+  
+  public interface Example {
+    double action(double id);
+  }
+  
+  public static class ExampleImpl implements Example {
+    public double action(double val) {
+      return Math.exp(val * Math.cosh(val));
+    }
+  }
+  
+  public static class SyncExampleImpl implements Example {
+    public synchronized double action(double val) {
+      return Math.exp(val * Math.cosh(val));
+    }
+  }
+  
+  /**
+   * @param tested
+   * @param repeats
+   */
+  public ExampleBenchmark(String name, Example tested, int repeats, int warmup) {
+    this.name = name;
+    this.tested = tested;
+    this.repeats = repeats;
+    this.warmup = warmup;
+  }
+  
+  /**
+   * @param baseline
+   * @return
+   */
+  public int benckmark(long baseline) {
+    long time = time();
+    
+    long diff = time - baseline;
+    double ratio = ((double) diff)/baseline;
+    int perc = (int) (100*ratio);
+        
+    System.out.printf("%s (%8.2f ns/call): %d%% diff with baseline\n", name, (double)time, perc);
+    return perc;
+  }
+  
+  /**
+   * @return
+   */
+  public long time() {
+    act(warmup);
+    return act(repeats);
+  }
+  
+  /**
+   * @param repeats
+   * @param random
+   */
+  private long act(int repeats) {
+    System.gc();
+    
+    double point = 0;
+    double step = 1f/repeats;
+    
+    long now = System.nanoTime();
+    for (int i = 0; i < repeats; i++, point +=step) {
+      tested.action(point);
+    }
+    return (System.nanoTime() - now) / repeats;
+  }
+}
